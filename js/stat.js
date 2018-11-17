79f92de51026de73ea0e827040315b785efad4c4
Crow26 <7tacson7@gmail.com>
2018-11-17 14:13:23 +0300

    Выполнена реализация показа окна, в котором будет отображаться статистика игры

---
+++ js/stat.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var renderCloud = function(ctx, color, figure) {
+  ctx.beginPath();
+  ctx.moveTo(figure.startPosition.x, figure.startPosition.y);
+  
+  for(var i = 0; i < figure.points.length; i++) {
+    ctx.lineTo(figure.points[i].x, figure.points[i].y);
+  }
+  
+  ctx.closePath();
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+window.renderStatistics = function(ctx) {
+  var cloud = {
+    startPosition: {x: 100, y: 50},
+    points: [
+      {x: 500, y: 50},
+      {x: 550, y: 125},
+      {x: 500, y: 200},
+      {x: 100, y: 200},
+      {x: 50, y: 125},
+      {x: 100, y: 50}
+    ]
+  };
+  var shadowCloud = {
+    startPosition: {x: 110, y: 60},
+    points: [
+      {x: 510, y: 60},
+      {x: 560, y: 135},
+      {x: 510, y: 210},
+      {x: 110, y: 210},
+      {x: 60, y: 135},
+      {x: 110, y: 60}
+    ]
+  };
+  
+  renderCloud(ctx, 'rgba(0, 0, 0, 0.3)', shadowCloud);
+  renderCloud(ctx, '#ffffff', cloud);
+};
