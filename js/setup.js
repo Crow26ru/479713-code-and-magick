ec1405e3cd5beedee4d4a74f2fad4a8e95b70cf5
Crow26 <7tacson7@gmail.com>
2018-11-20 23:57:12 +0300

    Реализована функция, задающая цвет мантии волшебника

--- js/setup.js
+++ js/setup.js
@@ -43,4 +43,16 @@ var setFullname = function () {
   return surname + name;
 };
 
+var setCoatColor = function () {
+  var colors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
 showModalSetup();
