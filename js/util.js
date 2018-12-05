60edadd63e99ab1487ff28e2a1e7e8b4ba17cc9a
Crow26 <7tacson7@gmail.com>
2018-12-05 15:04:43 +0300

    ESLint: исправлены ошибки

--- js/util.js
+++ js/util.js
@@ -7,7 +7,7 @@
   var getRandomElementArray = function (arr) {
     return arr[Math.floor(Math.random() * arr.length)];
   };
-  
+
   window.util = {
     ENTER_KEYCODE: ENTER_KEYCODE,
     ESC_KEYCODE: ESC_KEYCODE,
